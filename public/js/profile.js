184e25864958ab0a9fbab808fee1ad0ae0908f5c
Brooke Maneotis <xmaneotisb@yahoo.com>
2020-12-21 22:06:40 -0700

    changed comments

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,15 +1,9 @@
-//querySelector('#project-name') - blog-name
-//const description #project-desc - - const post = document.querySelector('#blog-post'
-//const response = await fetch(`/api/projects - api/blogs
-//.querySelector('.new-project-form' - new-blog-form
-//.querySelector('.project-list') - blog list
-
 
 const newFormHandler = async (event) => {
     event.preventDefault();
   
     const title = document.querySelector('#blog-name').value.trim();
-    //const needed_funding = document.querySelector('#project-funding').value.trim();
+    
     const post = document.querySelector('#blog-post').value.trim();
   
     if (title && post) {
